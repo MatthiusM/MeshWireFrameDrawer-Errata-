9270a83d050b7e76bcf445c5734e8b1695e51a01
MatthiusM <91685851+MatthiusM@users.noreply.github.com>
2023-02-14 12:41:20 +0000

    Line sweep done

--- main.cpp
+++ main.cpp
@@ -5,89 +5,93 @@
 #include "geometry.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red   = TGAColor(255, 0,   0,   255);
-Model *model = NULL;
-const int width  = 800;
-const int height = 800;
+const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
+const int width = 1080;
+const int height = 1080;
 
 TGAColor RandomColour() {
-    return TGAColor(rand() % 256, rand() % 256, rand() % 256, 255);;
+	return TGAColor(rand() % 256, rand() % 256, rand() % 256, 255);;
 }
 
-template <typename t>
-void swap(t* a, t* b) {
-    t temp = *b;
-    *b = *a;
-    *a = temp;
+void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color) {
+	bool steep = false;
+	if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {
+		std::swap(p0.x, p0.y);
+		std::swap(p1.x, p1.y);
+		steep = true;
+	}
+	if (p0.x > p1.x) {
+		std::swap(p0, p1);
+	}
+
+	for (int x = p0.x; x <= p1.x; x++) {
+		float t = (x - p0.x) / (float)(p1.x - p0.x);
+		int y = p0.y * (1. - t) + p1.y * t;
+		if (steep) {
+			image.set(y, x, color);
+		}
+		else {
+			image.set(x, y, color);
+		}
+	}
 }
 
-// Write the line method here
-void line(int x0, int y0, int x1, int y1, TGAImage* image, TGAColor colour) {
-    bool steep = false;
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
+	if (t0.y == t1.y && t0.y == t2.y) { return; }
 
-    if (abs(x0 - x1) < abs(y0 - y1)) {
-        swap(&x0, &y0);
-        swap(&x1, &y1);
-        steep = true;
-    }
+	if (t0.y > t1.y) { std::swap(t0, t1); }
+	if (t0.y > t2.y) { std::swap(t0, t2); }
+	if (t1.y > t2.y) { std::swap(t1, t2); }
+	float total_height = t2.y - t0.y;
+	for (float y = t0.y; y < t1.y; y++) {
+		float segment_height = t1.y - t0.y + 1;
+		float alpha = (y - t0.y) / total_height;
+		float beta = (y - t0.y) / segment_height;
+		Vec2i a = t0 + (t2 - t0) * alpha;
+		Vec2i b = t0 + (t1 - t0) * beta;
 
-    if (x0 > x1) {
-        swap(&x0, &x1);
-        swap(&y0, &y1);
-    }
+		if (a.x > b.x) { std::swap(a, b); }
 
+		for (float j = a.x; j < b.x; j++) {
+			image.set(j, y, color);
+		}
+		//image.set(a.x, y, red);
+		//image.set(b.x, y, green);
+	}
+	for (float y = t1.y; y < t2.y; y++) {
+		float segment_height = t2.y - t1.y + 1;
+		float alpha = (y - t0.y) / total_height;
+		float beta = (y - t1.y) / segment_height;
+		Vec2i a = t0 + (t2 - t0) * alpha;
+		Vec2i b = t1 + (t2 - t1) * beta;
+		
+		if (a.x > b.x) { std::swap(a, b); }
 
-
-    for (float x = x0; x <= x1; x++) {
-        float t = (x - x0) / (x1 - x0);
-        float y = y0 * (1 - t) + (y1 * t);
-
-        if (steep) {
-            image->set(y, x, colour);
-        }
-        else {
-            image->set(x, y, colour);
-        }
-
-    }
-
-
+		for (float j = a.x; j < b.x; j++) {
+			image.set(j, y, color);
+		}
+		//image.set(b.x, y, green);
+		//image.set(a.x, y, red);
+	}
+	line(t0, t1, image, color);
+	line(t1, t2, image, color);
+	line(t2, t0, image, color);
 }
-
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage* image, TGAColor color) {
-    if (t0.y > t1.y) { swap(&t0, &t1); }
-    if (t0.y > t2.y) { swap(&t0, &t2); }
-    if (t1.y > t2.y) { swap(&t1, &t2); }
-    int totalHeight = t2.y - t0.y;
-    for (int y = t0.y; y < t1.y; y++) {
-        int segmentHeight = t1.y - t0.y + 1;
-        int alpha = (y - t0.y) / totalHeight;
-        int beta = (y - t0.y) / segmentHeight;
-        Vec2i a = (t0 + (t2 - t0) * alpha);
-        Vec2i b = (t0 + (t1 - t0) * beta);
-        image->set(a.x, y, white);
-        image->set(b.x, y, white);
-    }
-    line(t0.x, t0.y, t1.x, t1.y, image, color);
-    line(t1.x, t1.y, t2.x, t2.y, image, color);
-    line(t2.x, t2.y, t0.x, t0.y, image, color);
-}
-
-
 
 int main(int argc, char** argv) {
-    srand(time(0));
-    Vec2i t0[3] = { Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80) };
-    Vec2i t1[3] = { Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180) };
-    Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
+	TGAImage image(width, height, TGAImage::RGB);
 
-    TGAImage image(width, height, TGAImage::RGB);
-    triangle(t0[0], t0[1], t0[2], &image, RandomColour());
-    triangle(t1[0], t1[1], t1[2], &image, RandomColour());
-    triangle(t2[0], t2[1], t2[2], &image, RandomColour());
-    image.flip_vertically(); // we want to have the origin at the left bottom corner of the image
-    image.write_tga_file("output.tga");
-    delete model;
-    return 0;
+	Vec2i t0[3] = { Vec2i(650, 1000),   Vec2i(530, 1066),  Vec2i(968, 901) };
+	Vec2i t1[3] = { Vec2i(90, 680),  Vec2i(42, 750),   Vec2i(895, 627) };
+	Vec2i t2[3] = { Vec2i(360, 380), Vec2i(1050, 160), Vec2i(1079, 256) };
+
+	triangle(t0[0], t0[1], t0[2], image, red);
+	triangle(t1[0], t1[1], t1[2], image, white);
+	triangle(t2[0], t2[1], t2[2], image, green);
+
+	image.flip_vertically(); // Origin at the left bottom corner of the image
+	image.write_tga_file("output.tga");
+	return 0;
 }
 
